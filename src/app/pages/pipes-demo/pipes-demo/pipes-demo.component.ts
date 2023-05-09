c973d367ca775548569a761873c64cfe036df34c
Govindram Solanki <gram01846@gmail.com>
2023-05-09 15:53:17 +0530

    Pipes COmplete

--- src/app/pages/pipes-demo/pipes-demo/pipes-demo.component.ts
+++ src/app/pages/pipes-demo/pipes-demo/pipes-demo.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class PipesDemoComponent {
   birthday = new Date(1988, 3, 15); // April 15, 1988
+  name = 'heet';
+  trainees = [
+    { name: 'Rohit', age: 18 },
+    { name: 'Heet', age: 18 },
+  ];
 }
